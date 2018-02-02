113100bf2a13de228227cb64c6afa7da95b8eaa0
LeFichBoy <Le.fichbo@gmail.com>
2018-02-02 15:31:14 +0200

    sadf
    asdf

--- bot.js
+++ bot.js
@@ -2,8 +2,9 @@ const bs = require("./setting.json");
 const prf = bs.prf;
 const discord = require ("discord.js");
 const bot = new discord.Client;
+const moment = require("moment");
 let b = "";
-
+var array1
 
 // expected output: "a"
 // expected output: "b"
@@ -18,32 +19,38 @@ bot.login(bs.token);
 bot.on("message", async message => {
 	if (message.author.bot) return;
 	if (message.channel.type === "dm") return;
-	var ggwp = bot.channel.find("406371324865544205");
-	bot.sendMessage(ggwp, "lelle");
+
 	
 	let msaray = message.content.split(" ");
 	if (!message.content.startsWith(prf)) return;
 	
 	let cm = msaray[0];
 	let args = msaray.slice(1);
-	let info = args[1];
-	var array1 = info.split("");
+	let info = args[0];
+
 	
 	if (cm === `${prf}bg`) {
-		
-
-		array1.forEach(function(element) {
-			b = b + element;
+		message.delete();
+		let array1 = info.split("");
+		let arry = array1;
+		array1 = [];
+		arry.forEach(function(element) {
+			if (isNaN(element))b = b + ":regional_indicator_" + element + ": ";
+			else if (element === "0") b = b + ":zero:";
+			else if (element === "1") b = b + ":one:";
+			else if (element === "2") b = b + ":two:";
+			else if (element === "3") b = b + ":three:";
+			else if (element === "4") b = b + ":four:";
+			else if (element === "5") b = b + ":five:";
+			else if (element === "6") b = b + ":six:";
+			else if (element === "7") b = b + ":seven:";
+			else if (element === "8") b = b + ":eight:";
+			else b = b + ":nine:";
 		});
 		
 		message.channel.sendMessage(b);
+		b = "";
 	};
-	
-	console.log(msaray);
-	console.log(cm);
-	console.log(args);
-	
-	
 });
 
 
